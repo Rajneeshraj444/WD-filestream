903bf471ff73b90aef1ab34b8bb3fec5879f2b4d
Rajneeshraj444 <Kumarrajnish30097@gmail.com>
2025-09-16 22:57:57 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "09814685583:AAH_nJs7-OKiFuHkYedfhrQV5LjkCB5g"; // Insert your bot token.
+const BOT_TOKEN = "8354770467:AAGQ6xx5KKdl31_Jfn-fi24QAwja_Ad0YM8"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
-const BOT_OWNER = 0064350805; // Insert your telegram account id.
-const BOT_CHANNEL = -10020358474134; // Insert your telegram channel id which the bot is admin in.
+const BOT_OWNER = 5915871770; // Insert your telegram account id.
+const BOT_CHANNEL = -1002941291275; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 1234; // Insert a random integer number and keep it safe.
 
 // ----------Ohh Bhai Do Not Modify ---------- // 
